2ba266853a4359f5c1c7b7f31e8d63d5b9a7e89c
daniel <daniellsmarta@gmail.com>
2019-11-29 14:55:38 +0100

    building a simple parser

--- tr/main.py
+++ tr/main.py
@@ -1,4 +1,28 @@
 import yaml
+import argparse
+from termcolor import colored
+VERSION = 1.0
+print(
+    colored(
+        '''
+  _____           __  __              _____      _____      __        __ 
+ |  __ \         |  \/  |     /\     |  __ \    |  __ \    / /       /_ |
+ | |__) |   ___  | \  / |    /  \    | |__) |   | |  | |  / /_        | |
+ |  _  /   / _ \ | |\/| |   / /\ \   |  ___/    | |  | | | '_ \       | |
+ | | \ \  |  __/ | |  | |  / ____ \  | |        | |__| | | (_) |  _   | |
+ |_|  \_\  \___| |_|  |_| /_/    \_\ |_|        |_____/   \___/  (_)  |_|
+                                                                         
+                                                      
+                                                      
+                                                      ''', "blue"))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-V", "--version", help="show program version", action="store_true")
+# parser.add_argument()
+
+args = parser.parse_args()
+if args.version:
+    print("current version {}".format(VERSION))
 
 # Read YAML file
 try:
